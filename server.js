a6725dc39fd5b44a78db8cc1cfae07d0a7d8791f
Tyler <tylersayvetz@gmail.com>
2020-01-14 13:03:42 -0800

    added obj response for location request, see map bug in front end

--- server.js
+++ server.js
@@ -11,17 +11,24 @@ const PORT = process.env.PORT || 3001;
 //configure express
 app.use(cors());
 
-// //constructor function
-// const Location = function(city) {
-//   //get the data
-//   const geoData = require('./data/geo.json')[0]; 
-//   console.log(geoData)
+//constructor function
+const Location = function(city) {
+  //get the data
+  try { 
+  const geoData = require('./data/geo.json')[0]; 
+  // build the object
+  this.search_query = city;
+  this.formatted_location = geoData.display_name;
+  this.lattitude = geoData.lat;
+  this.longitude = geoData.lon;
+  console.log(this)
+  }
+  catch (err) {
+    console.log(error);
+  }
 
-
-  //build the object
-  // this.search_query = city;
-
-// }
+  
+}
 
 
 
@@ -34,28 +41,26 @@ app.use(cors());
 
 
 
-// //routes
-// app.get('/', (req, res) => {
-//   // console.log(req.query);
-//   // console.log(req.params); 
-//   console.log('Im alive');
-//   res.status(200).send('Server is alive');
-// })
+//routes
+app.get('/', (req, res) => {
+  // console.log(req.query);
+  // console.log(req.params); 
+  console.log('Im alive');
+  res.status(200).send('Server is alive');
+})
 
 app.get('/location', (req,res) => {
   console.log('hi!');
   const reqCity = req.query.city;
-  // const responseObj = new Location(reqCity);
-  console.log('made it past constructor');
-  res.send('It got to the location route!');
+  const responseObj = new Location(reqCity);
+  console.log("in route: ",responseObj);
+  res.status(200).send(responseObj);
 
 })
 
-// app.get('*',(req,res) => {
-//   res.status(404).send('that route cannot be found');
-// })
-
-
+app.get('*',(req,res) => {
+  res.status(404).send('that route cannot be found');
+})
 
 //configure port
 app.listen(PORT, () => {console.log(`Your server is listening on ${PORT}`)});
