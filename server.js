f3e69880c5a771ab6155410d21471891b35a062a
Tyler <tylersayvetz@gmail.com>
2020-01-14 10:10:53 -0800

    initial lab setup, server dependencies added

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 'use strict'
 
 const express = require('express');
-const env = require('dotenv');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
 
+const PORT = process.env.PORT || 3001;
+
+//configure port
+app.listen(PORT, () => {console.log(`'Your server is listening on ${PORT}`)});
+
+//configure express
+app.use(cors());
 
