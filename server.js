e16b65f973756892e878c35ff6c6fae3f1ab502b
harlen lopez <harlen.d.lopez@gmail.com>
2020-01-15 12:22:56 -0800

    reworked functions and started to add weather data

--- server.js
+++ server.js
@@ -11,56 +11,111 @@ const PORT = process.env.PORT || 3001;
 //configure express
 app.use(cors());
 
-//constructor function
-const Location = function(city) {
-  //get the data
-  try { 
-  const geoData = require('./data/geo.json')[0]; 
-  // build the object
-  this.search_query = city;
-  this.formatted_query = geoData.display_name;
-  this.latitude = geoData.lat;
-  this.longitude = geoData.lon;
-  console.log(this)
-  }
-  catch (err) {
-    console.log(error);
-  }
+//define routes
+app.get('/', homeHandler);
+app.get('/location', locationHandler);
+app.get('/weather', weatherHandler);
 
-  
+//route handlers
+
+
+
+//LOCATION
+
+function homeHandler(req,res) {
+  res.status(200).send('Server is alive this is the home page');
+}
+
+function locationHandler(req,res) {
+    // res.status(200).send('Server is alive this is the location page');
+    //get the data
+  const geoData = require('./data/geo.json'); 
+  const city = req.query.city;
+  console.log(req);
+    //run the data through the constructor and push to new array
+  const locationData = new Location(city, geoData);
+    //send the new array back to the front end 
+  res.status(200).json(locationData);
+
+}
+//constructor
+function Location(city, geoData) {
+  this.search_query = city;
+  this.formatted_query = geoData[0].display_name;
+  this.latitude = geoData[0].lat;
+  this.longitude = geoData[0].lon;
+  console.log(this)
+
 }
 
 
 
+//WEATHER
+function weatherHandler(req,res) {
+      // res.status(200).send('Server is alive this is the weather page');
+    const weatherData = require('./data/darksky.json');
+    
 
-/* {
-  search_query: 'Lynwood'
-  formatted_query: etc etc...'
-}}
-*/
+}
+
+//CONSTRUCTOR
+function Weather(daily){
+
+}
+
+//constructor function
+
+// const Weather = function(city){
+//   try{
+//     const weatherdata = require('./data/darksky.json/daily/data')[0];
+
+//     console.log(weatherdata);
+//     //build the object
+//     this.search_query = city;
+//     this.formatted_query = weatherdata.summary;
+    
+    
+//   } 
+//   catch (err) {
+//     console.log(error);
+//   }
+// }
+
+// /* {
+//   search_query: 'Lynwood'
+//   formatted_query: etc etc...'
+// }}
+// */
 
 
 
-//routes
-app.get('/', (req, res) => {
-  // console.log(req.query);
-  // console.log(req.params); 
-  console.log('Im alive');
-  res.status(200).send('Server is alive');
-})
+// //routes
 
-app.get('/location', (req,res) => {
-  console.log('hi!');
-  const reqCity = req.query.city;
-  const responseObj = new Location(reqCity);
-  console.log("in route: ",responseObj);
-  res.status(200).send(responseObj);
+// app.get('/location', (req,res) => {
+//   console.log('hi!');
+//   const reqCity = req.query.city;
+//   const responseObj = new Location(reqCity);
+//   console.log("in route: ",responseObj);
+//   res.status(200).send(responseObj);
+// })
 
-})
 
-app.get('*',(req,res) => {
-  res.status(404).send('that route cannot be found');
-})
 
-//configure port
+// app.get('/weather', (req,res) => {
+//   console.log('weather working');
+
+
+
+
+//   const reqCity = req.query.city;
+//   const responseObj = new Weather(reqCity);
+//   console.log("in transit", responseObj);
+//   res.status(200).send(responseObj);
+// })
+
+// app.get('*',(req,res) => {
+//   res.status(404).send('that route cannot be found');
+// })
+
+// //configure port
 app.listen(PORT, () => {console.log(`Your server is listening on ${PORT}`)});
