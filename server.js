8e65479a413d0dc9a6fa38334610d24ab7ea1a32
harlen lopez <harlen.d.lopez@gmail.com>
2020-01-16 09:45:01 -0800

    weather data populating

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 'use strict'
-
+//express library is my server
 const express = require('express');
+// const pg = require('pg');
+//const client = new pg.client(process.env.DATABASE_URL);
+//client.on('error', err => console.error(err));
 const app = express();
+//allows our server talk to the front end
 const cors = require('cors');
 require('dotenv').config();
-
-const PORT = process.env.PORT || 3001;
-
-
-//configure express
 app.use(cors());
+const PORT = process.env.PORT || 3001;
 
 //define routes
 app.get('/', homeHandler);
 app.get('/location', locationHandler);
 app.get('/weather', weatherHandler);
-
-//route handlers
-
-
 
 //LOCATION
 
@@ -53,17 +49,27 @@ function Location(city, geoData) {
 //WEATHER
 function weatherHandler(req, res) {
   // res.status(200).send('Server is alive this is the weather page');
-
+  try{
   const weatherData = require('./data/darksky.json');
   let weatherDataArray = [];
   let weatherArray = weatherData.daily.data;
+  console.log('weather data',weatherArray);
   // console.log('we are in handler',weatherArray);
-  weatherDataArray = weatherArray.map((obj) => {
-    new Weather(obj);
-  })
+   weatherArray.forEach((obj) => {
+    let day = new Weather(obj);
+    weatherDataArray.push(day);
+    // console.log('this is the day log',day);
+
+  });
+
   console.log('this is the new array', weatherDataArray);
   res.send(weatherDataArray);
-  res.status(200).json(weatherDataArray);
+  // res.status(200).json(weatherDataArray);
+  }
+    catch (err) {
+    console.log(err);
+  }
+
 }
 
 
@@ -127,6 +133,6 @@ function Weather(daily) {
 // app.get('*',(req,res) => {
 //   res.status(404).send('that route cannot be found');
 // })
-
 // //configure port
 app.listen(PORT, () => { console.log(`Your server is listening on ${PORT}`) });
+
