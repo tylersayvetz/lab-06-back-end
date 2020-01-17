a79cdf52be8b58014bcb2d4fe44132a0f1fcf53e
Tyler <tylersayvetz@gmail.com>
2020-01-17 10:35:41 -0800

    finish DB cacheing of location data

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 'use strict'
 //express library is my server
 const express = require('express');
-// const pg = require('pg');
-//const client = new pg.client(process.env.DATABASE_URL);
-//client.on('error', err => console.error(err));
+const pg = require('pg');
 const app = express();
-//allows our server talk to the front end
 const cors = require('cors');
 const superagent = require('superagent');
 require('dotenv').config();
-const pg = require('pg');
+
 
 //DB config
 const client = new pg.Client(process.env.DB_URL);
@@ -31,28 +28,6 @@ const cachedLocations = [];
 
 
 //----------Functions and const area ----------
-const findCity = (req, res, next) => {
-  //does the searched city exist? if not redirect to /error
-  console.log("findCity");
-  const city = req.query.city;
-  try {
-    const rawData = require('./data/geo.json');
-    const valid = rawData.find(cit => {
-      const foundCity = cit.display_name.split(',');
-      return (foundCity[0].toLowerCase() === city) ? city : null;
-    })
-    if (valid) {
-      next();
-    }
-    else {
-      send500(req, res);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-
-}
-
 const send500 = (req, res) => {
   res.status(500).json({
     status: 500,
@@ -96,7 +71,12 @@ const DBSelect = (selection) => {
     client.query(query, values)
       .then(results => {
         console.log('DB "select" successful', results);
-        resolve(results);
+        if (results.rows.length > 0) {
+          console.log('DB found one or more rows that matched the query')
+          resolve(results.rows[0]);
+        } else {
+          reject('DB Didnt find any city that matched the query')
+        }
       })
       .catch(error => {
         console.log('DB "select" method unsuccessful', error)
@@ -104,10 +84,10 @@ const DBSelect = (selection) => {
   })
 }
 
-const DBInsert = () => {
+const DBInsertLocations = (search_query, formatted_query, latitude, longitude) => {
   return new Promise((resolve, reject) => {
-    const query = '';
-    const values = selection;
+    const query = 'INSERT INTO city_explorer_locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4)';
+    const values = [search_query.toLowerCase(), formatted_query, latitude, longitude];
     client.query(query, values)
       .then(results => {
         console.log('DB "insert" successful', results);
@@ -116,76 +96,70 @@ const DBInsert = () => {
       .catch(error => console.log('DB "insert" method unsuccessful', error));
   })
 }
+
 //----------routes----------
 app.get('/', (req, res) => {
   console.log('Im alive');
   res.status(200).send('Server is alive');
 })
 
-app.get('/location', (req, res,) => {
+app.get('/location', (req, res, ) => {
   console.log('hi!');
-  const reqCity = req.query.city;
+  const reqCity = req.query.city.toLowerCase();
+  let responseSent = false;
 
-  //look for a cached location that matches the query city.
-
-  const cachedObj = cachedLocations.find(location => {
-    return location.search_query.toLowerCase() === req.query.city ? location : undefined;
-  })
-//SELECT the db for a city that matches
-  //if it is there, send it to front end
-  //if it is not there, iNSERT into database
-//TODO: WORKING HERE! BUILD INSERT FUNCTION
-  // DBSelect('Proof')
-  //   .then(results => {
-  //     console.log('DB results!: ', results.rows[0])
-  //   })
-  //   .catch(error => console.log('broke'))
-
-  if (cachedObj) {
-    res.status(200).json(cachedObj);
-  } else {
-    //otherwise, make a superAgent request and get that location.
-    superagent.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATION_IQ}&q=${reqCity}&format=json`)
-      .then((results) => {
-        //then construct a new Location with the results.
-        const responseObj = new Location(results.body[0]);
-        // cache the resultant object.
-        cachedLocations.push(responseObj);
-        // send the obj.
-    
-        res.status(200).json(responseObj);
-      })
-      .catch((error) => {
-        console.log('Promise failed');
-      })
-  }
-
-app.get('/weather', (req, res) => {
-  superagent.get(`https://api.darksky.net/forecast/${process.env.DARK_SKY}/${req.query.latitude},${req.query.longitude}`)
-    .then((results) => {
-      const responseObj = new Weather(results.body);
-      console.log(responseObj.weather);
-      res.status(200).json(responseObj.weather);
+  DBSelect(reqCity)
+    .then(results => {
+      console.log('DB results!: ', results)
+      res.status(200).json(results);
+      responseSent = true;
+      //TODO: how do I terminate the execution of the route here? 
     })
-})
-
-app.get('/events', (req, res) => {
-
-  superagent.get(`http://api.eventful.com/json/events/search?app_key=${process.env.EVENTFUL}&location=${req.query.search_query}&sort_order=date&date=Future&page_size=30&page_number=1`)
-    .then((results) => {
-      console.log(JSON.parse(results.text));
-      const parsed = JSON.parse(results.text).events;
-      if (!parsed) {
-        send500(req, res);
-      } else {
-        const responseObj = new Events(parsed.event.slice(0, 20));
-        // console.log(responseObj.events)
-        res.status(200).json(responseObj.events);
+    .catch((error) => {
+      console.log('DB didnt fint anything..hitting API..');
+      return superagent.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATION_IQ}&q=${reqCity}&format=json`)
+    })
+    .then(results => {
+      //then construct a new Location with the results.
+      //TODO: this is a terrible solution... 
+      if (!responseSent) {
+        const responseObj = new Location(results.body[0]);
+        //insert new data into DB
+        DBInsertLocations(responseObj.search_query, responseObj.formatted_query, responseObj.latitude, responseObj.longitude)
+          .then(results => console.log('city inserted into DB'))
+          .catch(error => console.log('There was an error inserting data into DB', error))
+        res.status(200).json(responseObj);
       }
     })
     .catch(error => {
-      console.log(error);
-      console.log();
+      console.log('error getting data from API', error);
     })
+
+  app.get('/weather', (req, res) => {
+    superagent.get(`https://api.darksky.net/forecast/${process.env.DARK_SKY}/${req.query.latitude},${req.query.longitude}`)
+      .then((results) => {
+        const responseObj = new Weather(results.body);
+        res.status(200).json(responseObj.weather);
+      })
+  })
+
+  app.get('/events', (req, res) => {
+
+    superagent.get(`http://api.eventful.com/json/events/search?app_key=${process.env.EVENTFUL}&location=${req.query.search_query}&sort_order=date&date=Future&page_size=30&page_number=1`)
+      .then((results) => {
+        console.log(JSON.parse(results.text));
+        const parsed = JSON.parse(results.text).events;
+        if (!parsed) {
+          send500(req, res);
+        } else {
+          const responseObj = new Events(parsed.event.slice(0, 20));
+          // console.log(responseObj.events)
+          res.status(200).json(responseObj.events);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        console.log();
+      })
   })
 })
